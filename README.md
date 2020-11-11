b4d085bedd14332663c3fbe0d38857c182ef86f8
Yuki Abe <12268087+yukia3@users.noreply.github.com>
2020-11-11 10:42:52 +0900

    Update cost-calculation and README

--- README.md
+++ README.md
@@ -1,15 +1,43 @@
 # mecab-initial-cost-calculation
 MeCabのカスタム辞書を作成する際に算出が必要となる単語生起コストについて、算出するPythonプロジェクト。
 
-## カスタム辞書作成方法
+## カスタム辞書作成方法（cost-calculation活用）
+### コスト計算
 ``` sh
-/usr/lib/mecab/mecab-dict-index -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd -u ${dic_name}.dic -f utf8 -t utf8 ${moto}.csv
+python3 cost-calculation.py -f ${moto}.csv -o ${cost_calculated}.csv
 ```
 
+### 辞書ファイル作成
+``` sh
+/usr/lib/mecab/mecab-dict-index -d /usr/share/mecab/dic/ipadic -u ${dic_name}.dic -f utf-8 -t utf-8 ${cost_calculated}.csv
+
+/usr/lib/mecab/mecab-dict-index -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd -u ${dic_name}.dic -f utf-8 -t utf-8 ${moto}.csv
+```
+※ `sudo find  /usr/ -name rewrite.def`
+
+### mecabrcへの定義
 ```sh
 vim /etc/mecabrc
+vim /usr/local/etc/mecabrc
 ```
 
 ```
-userdic = /root/mecab-initial-cost-calculation/${dic_name}.dic
+userdic = /root/mecab-initial-cost-calculation/${dict_name}.dic
 ```
+
+### 実行時指定
+```
+mecab -u ${dict_name}.dic
+mecab -u /root/mecab-initial-cost-calculation/${dict_name}.dic,/root/mecab-initial-cost-calculation/${dict_name} -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd -Odump
+```
+http://www.mwsoft.jp/programming/nlp/mecab_dictionary_customize.html
+
+
+## コスト自動推定
+### モデル
+https://taku910.github.io/mecab/dic.html より
+[ダウンロード](https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7bnc5aFZSTE9qNnM)
+
+### 辞書データ
+https://taku910.github.io/mecab/ よりIPA 辞書,
+[ダウンロード](https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM) 
--- src/cost-calculation.py
+++ src/cost-calculation.py
@@ -4,48 +4,47 @@ import argparse
 import re
 
 # Average cost per char count by below command
-# https://sourceforge.net/projects/mecab/files/mecab-ipadic/2.7.0-20070801/
 # cat Noun.*.csv | perl -nle 'use Encode;@a=split(/,/);($word,$l_id,$r_id,$cost)=@a[0,1,2,3];$len=length(Encode::decode(q{utf8},$word));$e=$rslt->[$len]||={};$e->{sum}+=$cost;$e->{n}++;$e->{max}=$cost if ($e->{max}<$cost);$e->{min}=$cost if (!defined($e->{min})or $cost<$e->{min});END{print "{"; for $len (0..$#$rslt){$e=$rslt->[$len]||{};print "	$len:".$e->{sum}/$e->{n}."," if $e->{n}}; print "}" }';
 AVG_COST_PER_NOUN_CHAR_COUNT = {
-  1:8882.12813370473,
-  2:8105.86201834862,
-  3:7672.59370837577,
-  4:7825.51551655864,
-  5:8082.21840480491,
-  6:8080.35633540136,
-  7:7648.4871908913,
-  8:7655.57659688674,
-  9:7449.21291053228,
-  10:7261.52115480338,
-  11:7067.8244737781,
-  12:6828.65093085106,
-  13:6542.04,
-  14:6573.94550063371,
-  15:6304.30240549828,
-  16:6228.82598039216,
-  17:6227.00911854103,
-  18:6116.61678832117,
-  19:6194.57062146893,
-  20:6172.064,
-  21:5902.47899159664,
-  22:6261.43333333333,
-  23:6025.80952380952,
-  24:6067.5,
-  25:5766.10714285714,
-  26:5509.90909090909,
-  27:5545.29166666667,
-  28:6237.45454545455,
-  29:5368.71428571429,
-  30:5122,
-  31:5107,
-  32:5122,
-  33:5122,
-  34:6849,
-  35:5122,
-  36:4437.5,
-  37:5122,
-  38:5122,
-  39:5122,
+  1: 8882.12813370473,
+  2: 8105.86201834862,
+  3: 7672.59370837577,
+  4: 7825.51551655864,
+  5: 8082.21840480491,
+  6: 8080.35633540136,
+  7: 7648.4871908913,
+  8: 7655.57659688674,
+  9: 7449.21291053228,
+  10: 7261.52115480338,
+  11: 7067.8244737781,
+  12: 6828.65093085106,
+  13: 6542.04,
+  14: 6573.94550063371,
+  15: 6304.30240549828,
+  16: 6228.82598039216,
+  17: 6227.00911854103,
+  18: 6116.61678832117,
+  19: 6194.57062146893,
+  20: 6172.064,
+  21: 5902.47899159664,
+  22: 6261.43333333333,
+  23: 6025.80952380952,
+  24: 6067.5,
+  25: 5766.10714285714,
+  26: 5509.90909090909,
+  27: 5545.29166666667,
+  28: 6237.45454545455,
+  29: 5368.71428571429,
+  30: 5122,
+  31: 5107,
+  32: 5122,
+  33: 5122,
+  34: 6849,
+  35: 5122,
+  36: 4437.5,
+  37: 5122,
+  38: 5122,
+  39: 5122,
 }
 
 # cat Verb.csv | perl -nle 'use Encode;@a=split(/,/);($word,$l_id,$r_id,$cost)=@a[0,1,2,3];$len=length(Encode::decode(q{utf8},$word));$e=$rslt->[$len]||={};$e->{sum}+=$cost;$e->{n}++;$e->{max}=$cost if ($e->{max}<$cost);$e->{min}=$cost if (!defined($e->{min})or $cost<$e->{min});END{print "{"; for $len (0..$#$rslt){$e=$rslt->[$len]||{};print "	$len:".$e->{sum}/$e->{n}."," if $e->{n}}; print "}" }';
@@ -119,7 +118,8 @@ AVG_COST_PER_ADVERB_CHAR_COUNT = {
 
 def convert_to_partial_format(csv_partial_words):
   partial_words_arr = csv_partial_words.split(',')
-  partial_words_arr = [partial_word + "\t*" for partial_word in partial_words_arr]
+  partial_words_arr = [partial_word +
+                       "\t*" for partial_word in partial_words_arr]
   expected = "{}\nEOS".format("\n".join(partial_words_arr))
   return expected
 
@@ -130,25 +130,35 @@ def normalize_surface(surface):
   return surface_normalized
 
 
-def run(csv_file_path, output_file_path):
+def run(csv_file_path, output_file_path, dict_path):
   rows = []
   with open(csv_file_path) as f:
     # reader = csv.reader(f, delimiter='\t')
     reader = csv.reader(f)
     rows = [row for row in reader]
 
+  # wakati1
+  if dict_path:
+    wakati1 = MeCab.Tagger("-d {}".format(dict_path))
+  else:
+    wakati1 = MeCab.Tagger()
+
+  # wakati2
+  if dict_path:
+    wakati2 = MeCab.Tagger("-p -d {}".format(dict_path))
+  else:
+    wakati2 = MeCab.Tagger("-p")
+
   for row in rows:
     # [表層形, 品詞, 品詞細分類1, 品詞細分類2, 品詞細分類3, 活用型, 活用形, 原形, 読み, 発音]
     word = row[0]
     pos = row[1]
     feature_arr = row[1:]
-    
+
     # ----- 不要な対象をチェックし除外する -------
     # 1単語に分かち書きされ、それが通常扱いである場合、単語登録済みのためskip
-    # wakati = MeCab.Tagger()
-    wakati = MeCab.Tagger(
-        '-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
-    real_node = wakati.parseToNode(word)
+
+    real_node = wakati1.parseToNode(word)
 
     real_nodes = []
     while real_node:
@@ -172,21 +182,18 @@ def run(csv_file_path, output_file_path):
       real_node = real_node.next
 
     if len(real_nodes) == 3:
-      if real_nodes[1].stat == 1:
+      if real_nodes[1].stat == 0:
         print("{}は既に登録済みです".format(word))
         continue
     # ----- 不要な対象をチェックし除外する -------
-    
+
     # ----- コスト評価（単体ワードとして分かち書きされる単語生起コスト算出） -------
     # 現時点の辞書での累積コストを取得
     real_acm_cost = real_nodes[-1].cost
-    
+
     # Partial条件で強制的に単体ワード分かち書きし、単体ワードのBOS/EOS連接コストを算出
-    # wakati = MeCab.Tagger("-p")
-    wakati = MeCab.Tagger(
-        "-p -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
     partial_text = convert_to_partial_format(word)
-    partial_node = wakati.parseToNode(partial_text)
+    partial_node = wakati2.parseToNode(partial_text)
 
     partial_nodes = []
     while partial_node:
@@ -200,11 +207,11 @@ def run(csv_file_path, output_file_path):
     partial_acm_cost = partial_nodes[-1].cost
     partial_word_cost = partial_nodes[1].wcost
     partial_connection_cost = partial_acm_cost - partial_word_cost
-    
+
     # 単体ワードとして分かち書きされるコストを算出
     # ref. http://blog.livedoor.jp/techblog/archives/65828235.html
     # best_word_cost = (real_acm_cost - partial_connection_cost - 1)
-    best_word_cost = (real_acm_cost - partial_connection_cost -1) * 0.7
+    best_word_cost = (real_acm_cost - partial_connection_cost - 1) * 0.7
 
     # ----- コスト評価（単語文字長からの平均コスト取得） -------
     if pos == '名詞':
@@ -215,6 +222,8 @@ def run(csv_file_path, output_file_path):
       avg_cost = AVG_COST_PER_ADJ_CHAR_COUNT[len(word)]
     elif pos == '副詞':
       avg_cost = AVG_COST_PER_ADVERB_CHAR_COUNT[len(word)]
+    else:
+      avg_cost = 10000
 
     # ----- コスト最終評価 -------
     new_wcost = best_word_cost if best_word_cost < avg_cost else avg_cost
@@ -222,34 +231,40 @@ def run(csv_file_path, output_file_path):
     # ----- dict用csv出力 -------
     lcattr = ''
     rcattr = ''
-    with open(output_file_path, 'a') as f:
-      writer = csv.writer(f)
-      
+    with open(output_file_path, 'a', newline="") as f:
+      writer = csv.writer(f, lineterminator='\n')
+
       # [表層形,左文脈ID,右文脈ID,コスト,品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音]
       writer.writerow([
-        normalize_surface(word),
-        lcattr,
-        rcattr,
-        new_wcost,
-        *feature_arr
+          normalize_surface(word),
+          lcattr,
+          rcattr,
+          new_wcost,
+          *feature_arr
       ])
-  
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Calc Initial Cost')
   parser.add_argument(
-    '-f',
-    '--csvFilePath',
-    help='CSVファイルパス',
-    required=True
+      '-f',
+      '--csvFilePath',
+      help='CSVファイルパス',
+      required=True
   )
   parser.add_argument(
-    '-o',
-    '--outputFilePath',
-    help='アウトプットファイルパス',
-    default='calc_result.csv'
+      '-o',
+      '--outputFilePath',
+      help='アウトプットファイルパス',
+      default='calc_result.csv'
+  )
+  parser.add_argument(
+      '-d',
+      '--dictPath',
+      help='辞書パス（/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd など）',
+      default=''
   )
   args = vars(parser.parse_args())
-  
-  run(csv_file_path=args['csvFilePath'], output_file_path=args['outputFilePath'])
+
+  run(csv_file_path=args['csvFilePath'],
+      output_file_path=args['outputFilePath'], dict_path=args['dictPath'])
